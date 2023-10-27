4ba1f6fac2ccfcf5528cc771b0687f5012e06ce4
Nova67 <innovagames75@gmail.com>
2023-10-26 23:07:13 -0400

    esttructura DBZ creada y anexada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 import { CounterModule } from './counter/counter.module';
 import { HeoresModule } from './heroes/heroes.module';
-
-import { AppComponent } from './app.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeoresModule],
+  imports: [BrowserModule, CounterModule, HeoresModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
